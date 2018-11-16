4a3e632210c00f1bea52004420639c4135f18068
jacobsilver2 <jacobsilver2@mac.com>
2018-11-16 10:47:37 -0500

    added createEvent mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const mutations = {};
+const Mutations = {
 
-module.exports = mutations;
+  async createEvent(parent, args, ctx, info) {
+    // TODO: check if they are logged in
+
+    const event = await ctx.db.mutation.createEvent({
+      data: {
+        ...args
+      }
+    }, info);
+    return event;
+  },
+
+
+};
+
+module.exports = Mutations;
