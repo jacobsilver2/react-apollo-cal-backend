ad60a0e0bc2cc09a27b9ec19141941802f224a2e
jacobsilver2 <jacobsilver2@mac.com>
2018-11-19 18:18:26 -0500

    added single event query

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -11,6 +11,20 @@ const Mutations = {
     return event;
   },
 
+  updateEvent(parent, args, ctx, info) {
+    // take a copy of the updates
+    const updates = { ...args };
+    // remove the id from the updates
+    delete updates.id;
+    //run the update method
+    return ctx.db.mutation.updateEvent({
+      data: updates,
+      where: {
+        id: args.id
+      }
+    }, info)
+  }
+
 
 };
 
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,7 +2,7 @@ const { forwardTo } = require('prisma-binding');
 
 const Query = {
   events: forwardTo('db'),
-
+  event: forwardTo('db'),
 };
 
 module.exports = Query;
