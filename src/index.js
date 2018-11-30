1b4045eb18de8277be58a6a15894f82082ef28f9
jacobsilver2 <jacobsilver2@mac.com>
2018-11-30 08:22:40 -0500

    safety

--- src/index.js
+++ src/index.js
@@ -20,6 +20,20 @@ server.express.use((req, res, next) => {
   next();
 })
 
+// create a middleware that populates the user on each request
+server.express.use(async (req, res, next) => {
+  // if they aren't logged in, skip
+  if (!req.userId) {
+    return next();
+  }
+  const user = await db.query.user(
+    {where: { id: req.userId }},
+    `{ id, permissions, email, name }`
+  );
+  req.user = user;
+  next();
+})
+
 server.start({
   cors: {
     credentials: true,
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require('prisma-binding');
+const { hasPermission } = require('../utils');
 
 const Query = {
   events: forwardTo('db'),
@@ -13,6 +14,17 @@ const Query = {
     }, info)
   },
   
+  async users(parent, args, ctx, info) {
+    // check if they are logged in
+    if (!ctx.request.userId) {
+      throw new Error ("You Are Not Logged In");
+    }
+    // check if the user has permissions to query all the users
+    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+    // if they do, query all the users
+    return ctx.db.query.users({}, info);
+  }
+
 };
 
 module.exports = Query;
